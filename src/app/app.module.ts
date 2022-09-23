c4d0db23b0d9528e8420bb9e3a896f7a9978d56d
Carlos Gioda <carlosgioda@192.168.1.3>
2022-09-22 22:55:02 -0300

    third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { StudentComponent } from './components/student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
